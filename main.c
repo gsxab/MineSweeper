e35e15b16a4c2d3b381e80172f8f76a4bc610259
gsxab <2072414837@qq.com>
2016-08-26 01:15:21 +0800

    2016-8-26
    Updated basic contents. Renamed the src file.

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
-#include<stdio.h>
+﻿#include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
 
-#define BETTER_PERFORMANCE
-#define WIN_CONSOLE
+#pragma warning(disable:4996)
+
+//#define WIN_CONSOLE
+
+#ifndef WIN_CONSOLE
+#include <Windows.h>
+#endif //WIN_CONSOLE
 
 typedef enum CellState{
 	UNTOUCHED = 0, TOUCHED, FLAGGED, QUESTION
 }CellState;
 
-typedef enum Flag{
-	UNINIT = 0, INIT, LOSE, WIN
+typedef enum GameFlag{
+	PREINIT = 0, INIT, LOSE, WIN
 }Flag;
-
-typedef enum FlagMode{
-    FLAG_NORMAL = 0, FLAG_QUES, NO_CANCEL
-}FlagMode;
 
 typedef struct{
 	unsigned st  : 2;
@@ -28,32 +29,51 @@ typedef struct{
 }CellInfo;
 
 /* The maximum size of the board : (BOARDW - 2) by (BOARDH - 2). */
-#define BOARDW 20
-#define BOARDH 20
+#define BOARDW 30
+#define BOARDH 30
+
 /* Infomation about the board. */
 CellInfo board[BOARDW][BOARDH];
-int boardwidth, boardheight, minenum;
+
 /* Game state. */
 Flag state;
+
 /* Whether question mark available. */
-FlagMode flagmode;
+struct settings{
+	unsigned flagmode : 1;
+	unsigned performance : 1;
+	unsigned boardwidth : 5;
+	unsigned boardheight : 5;
+	unsigned minenum : 5;
+}settings;
+
+/* Score related */
+time_t laststart;
+int oprcnt;
 
 void init();/** To initialize the whole game. */
 void resize(int w, int h, int b);/** To resize the board. */
 void binit();/** To initialize the board. */
 void mine(int x, int y);/** To create mines on the board. */
 void touch(int x, int y);/** To perform touch on the board. */
-void flag(int x, int y, FlagMode mode);/** To perform flag on the board. */
+void flag(int x, int y, int no_cancel);/** To perform flag on the board. */
 void lclick(int x, int y);/** To perform a left-click on the board. */
 void rclick(int x, int y);/** To perform a right-click on the board. */
-void aclick(int x, int y);/** To perform a left-and-right click. */
+void bclick(int x, int y);/** To perform a both-left-and-right click. */
 void exec();/** To input and execute commands. */
-void pchar(CellInfo c);/** To get the char to display. */
+void pchar1(CellInfo c); void pchar(CellInfo c);/** To print the char to display. */
 void print();/** To print current board. */
 void judge();/** To judge if the player has won. */
 
 int main(){
-	char c;int num1, num2, num3;
+#ifndef WIN_CONSOLE
+	HWND cmd = GetConsoleWindow();
+	HDC dc = GetDC(cmd);
+	HDC dc1 = CreateCompatibleDC(dc);
+	HBITMAP bitmap = (HBITMAP) LoadImage(GetModuleHandle(0), MAKEINTRESOURCE(103/*IDB_BITMAP2*/), IMAGE_BITMAP, 65, 65, LR_CREATEDIBSECTION);
+#endif
+	char c;
+	int num1, num2, num3;
 	init();
 	printf("Game start?(N/n for no, s for setup)\n");
 	while(1){
@@ -65,7 +85,7 @@ int main(){
 			resize(num1, num2, num3);
 			printf("Please input: whether question mark should be displayed when an r-click happens on a flagged square.");
 			scanf("%d", &num1);
-			flagmode = num1;
+			settings.flagmode = (num1 != 0);
 		}
 		binit();
 		print();
@@ -80,10 +100,10 @@ int main(){
 
 /** To initialize the whole game. */
 void init(){
-	boardwidth = boardheight = 10;
-	minenum = 10;
+	settings.boardwidth = settings.boardheight = 10;
+	settings.minenum = 10;
 	srand((unsigned)time(NULL));
-	flagmode = FLAG_NORMAL;
+	settings.flagmode = settings.performance = 1;
 #ifdef WIN_CONSOLE
 	system("color 70");
 #endif
@@ -92,15 +112,15 @@ void init(){
 /** To resize the board. */
 void resize(int w, int h, int b){
 	if(w > 4 && w < BOARDW - 1 && h > 4 && h < BOARDH - 1){
-		boardwidth = w;
-		boardheight = h;
+		settings.boardwidth = w;
+		settings.boardheight = h;
 	} else {
 		printf("Invalid size!");
 		return;
 	}
 	if(b == 0)return;
 	if(b > w * h / 8 && b < w * h / 4){
-		minenum = b;
+		settings.minenum = b;
 	}  else {
 		printf("Invalid bomb number!");
 	}
@@ -108,37 +128,40 @@ void resize(int w, int h, int b){
 
 /** To initialize the board. */
 void binit(){
-    state = UNINIT;
 	int i, j;
-	for(i = 0; i <= boardheight + 1; i++){
-		for(j = 0; j <= boardwidth + 1; j++){
+    state = PREINIT;
+	for(i = 0; i <= settings.boardheight + 1; i++){
+		for(j = 0; j <= settings.boardwidth + 1; j++){
 			board[i][j].num = 0;
 			board[i][j].st = UNTOUCHED;
 		}
 	}
 }
 
-/** To create mines on the board. */
+/** To create mines on the board. To do on the first click. */
 void mine(int x, int y){
     int i, j, n, X, Y;
+	laststart = time(NULL);
 	state = INIT;
-	for(n = 0; n < minenum; ){
-		i = rand() % (boardwidth - 1) + 1;
-		j = rand() % (boardheight - 1) + 1;
+	for(n = 0; n < settings.minenum; ){
+		i = rand() % (settings.boardwidth - 1) + 1;
+		j = rand() % (settings.boardheight - 1) + 1;
 		if(board[i][j].num == 9)continue;
-		if(i == x && j == y)continue;
-		/* The first cell */
+		if(i == x && j == y)continue;/* No mine should be in the first cell touched. */
 		board[i][j].num = 9;
 		n++;
 	}
-	for(i = 1; i <= boardheight; i++){
-		for(j = 1; j <= boardwidth; j++){
+	for(i = 1; i <= settings.boardheight; i++){
+		for(j = 1; j <= settings.boardwidth; j++){
 			if(board[i][j].num == 9)continue;
 			n = 0;
-			for(X = -1; X <= 1; X++)
-				for(Y = -1; Y <= 1; Y++)
-					if( (X!=0 || Y!=0) && board[i + X][j + Y].num == 9)
+			for(X = -1; X <= 1; X++){
+				for(Y = -1; Y <= 1; Y++){
+					if( (X!=0 || Y!=0) && board[i + X][j + Y].num == 9){
 						n++;
+					}
+				}
+			}
 			board[i][j].num = n;
 		}
 	}
@@ -147,38 +170,36 @@ void mine(int x, int y){
 /** To perform touch on the board. */
 void touch(int x, int y){
 	int X, Y;
-	if(x <= 0 || x > boardheight || y <= 0 || y > boardwidth)return;
-	if(board[x][y].st == TOUCHED)return;
+	if(x <= 0 || x > settings.boardheight || y <= 0 || y > settings.boardwidth)return;
+	if(board[x][y].st != UNTOUCHED)return;
 	board[x][y].st = TOUCHED;
-	if(board[x][y].num == 0)
-		for(X = -1; X <= 1; X++)
+	if(board[x][y].num == 0){
+		for(X = -1; X <= 1; X++){
 			for(Y = -1; Y <= 1; Y++){
-				if( X!=0 || Y!=0 )
+				if( X!=0 || Y!=0 ){
 					touch(x + X, y + Y);
+				}
 			}
-	else if(board[x][y].num == 9)
+		}
+	}else if(board[x][y].num == 9){
 		state = LOSE;
+	}
 }
 
 /** To perform flag on the board. */
-void flag(int x, int y, FlagMode mode){
-	if(x <= 0 || x > boardheight || y <= 0 || y > boardwidth)return;
+void flag(int x, int y, int no_cancel){
+	if(x <= 0 || x > settings.boardheight || y <= 0 || y > settings.boardwidth)return;
 	switch(board[x][y].st){
 	case UNTOUCHED:
 		board[x][y].st = FLAGGED;
 		break;
 	case FLAGGED:
-		switch(mode){
-        case NO_CANCEL:
-            return;
-        case FLAG_NORMAL:
-            board[x][y].st = UNTOUCHED;
-            break;
-        case FLAG_QUES:
-            board[x][y].st = QUESTION;
-		}
+		if(no_cancel)break;
+		board[x][y].st = settings.flagmode ? UNTOUCHED : QUESTION;
+		break;
     case QUESTION:
-        board[x][y].st = FLAG_NORMAL;
+        board[x][y].st = UNTOUCHED;
+		break;
 	}
 }
 
@@ -191,20 +212,20 @@ void lclick(int x, int y){
 /** To perform a right-click on the board. */
 void rclick(int x, int y){
 	if(state >= LOSE)return;
-	flag(x, y, FLAG_NORMAL);
+	flag(x, y, 0);
 }
 
-/** To perform a left-and-right click. */
-void aclick(int x, int y){
+/** To perform a both-left-and-right click. */
+void bclick(int x, int y){
 	int X, Y;
 	int u=0, f=0;
-	if(x <= 0 || x > boardheight || y <= 0 || y > boardwidth)return;
+	if(x <= 0 || x > settings.boardheight || y <= 0 || y > settings.boardwidth)return;
 	if(board[x][y].st != TOUCHED)return;
     for(X = -1; X <= 1; X++)
         for(Y = -1; Y <= 1; Y++){
             if( X!=0 || Y!=0 ){
-                if(x + X <= 0 || x + X > boardheight ||
-                    y + Y <= 0 || y + Y > boardwidth)continue;
+                if(x + X <= 0 || x + X > settings.boardheight ||
+                    y + Y <= 0 || y + Y > settings.boardwidth)continue;
                 switch(board[x + X][y + Y].st){
                 case UNTOUCHED:
                     u++;
@@ -216,18 +237,19 @@ void aclick(int x, int y){
             }
         }
     if(f == board[x][y].num){
-        for(X = -1; X <= 1; X++)
+        for(X = -1; X <= 1; X++){
             for(Y = -1; Y <= 1; Y++){
                 if( X!=0 || Y!=0 ){
                     touch(x + X, y + Y);
                 }
             }
+		}
     }
     else if(u + f == board[x][y].num){
         for(X = -1; X <= 1; X++)
             for(Y = -1; Y <= 1; Y++){
                 if( X!=0 || Y!=0 ){
-                    flag(x + X, y + Y, NO_CANCEL);
+                    flag(x + X, y + Y, 1);
                 }
             }
     }
@@ -241,7 +263,7 @@ void exec(){
 	fflush(stdin);
 	fgets(buffer, 10, stdin);
 	sscanf(buffer, " %c %d %d", &c, &x, &y);
-	if(state == UNINIT)mine(x, y);
+	if(state == PREINIT)mine(x, y);
 	switch(c){
 	case 'L':
 	case 'l':
@@ -251,9 +273,9 @@ void exec(){
 	case 'r':
 		rclick(x, y);
 		return;
-    case 'A':
-    case 'a':
-        aclick(x, y);
+    case 'B':
+    case 'b':
+        bclick(x, y);
         return;
 	}
 }
@@ -264,39 +286,39 @@ void print(){
 #ifdef WIN_CONSOLE
 	system("cls");
 #endif
-#ifdef BETTER_PERFORMANCE
-    printf("┏");
-	for(j = 0; j < boardwidth - 1; j++)
-		printf("━┳");
-	printf("━┓\n");
-#endif
-	for(i = 1; i <= boardheight; i++){
-		for(j = 1; j <= boardwidth; j++){
-#ifdef BETTER_PERFORMANCE
-			printf("┃");
-			pchar(board[i][j]);
-#else
-			pchar(board[i][j]);
-#endif
-		}
-#ifdef BETTER_PERFORMANCE
-		printf("┃\n");
-		if(i != boardheight){
-            printf("┣");
-            for(j = 0; j < boardwidth - 1; j++)
-                printf("━╋");
-            printf("━┫\n");
-		}
-#else
-		printf("\n");
-#endif
+	if(settings.performance){
+		printf("┏");
+		for(j = 0; j < settings.boardwidth - 1; j++)
+			printf("━┳");
+		printf("━┓\n");
 	}
-#ifdef BETTER_PERFORMANCE
-	printf("┗");
-	for(j = 0; j < boardwidth - 1; j++)
-		printf("━┻");
-	printf("━┛\n");
-#endif
+	for(i = 1; i <= settings.boardheight; i++){
+		for(j = 1; j <= settings.boardwidth; j++){
+			if(settings.performance){
+				printf("┃");
+				pchar1(board[i][j]);
+			}else{
+				pchar(board[i][j]);
+			}
+		}
+		if(settings.performance){
+			printf("┃\n");
+			if(i != settings.boardheight){
+				printf("┣");
+				for(j = 0; j < settings.boardwidth - 1; j++)
+					printf("━╋");
+				printf("━┫\n");
+			}
+		}else{
+			printf("\n");
+		}
+	}
+	if(settings.performance){
+		printf("┗");
+		for(j = 0; j < settings.boardwidth - 1; j++)
+			printf("━┻");
+		printf("━┛\n");
+	}
 	if(state == WIN)
         printf("\nYou made it!\nPlay again?(n for no, s for setup)\n");
     else if(state == LOSE)
@@ -305,9 +327,8 @@ void print(){
         printf("\nPlease input the next command:\n");
 }
 
-/** To get the char to display. */
-#ifdef BETTER_PERFORMANCE
-void pchar(CellInfo c){
+/** To print the char to display. */
+void pchar1(CellInfo c){
 	switch(c.st){
 	case UNTOUCHED:
 		if(state >= LOSE && c.num == 9){
@@ -362,7 +383,6 @@ void pchar(CellInfo c){
 	}
 	return;
 }
-#else
 void pchar(CellInfo c){
 	switch(c.st){
 	case UNTOUCHED:
@@ -387,16 +407,15 @@ void pchar(CellInfo c){
         putchar('?');
 	}
 }
-#endif
 
 void judge(){
-    if(state >= LOSE)return;
     int i, j, n;
+    if(state >= LOSE)return;
     n = 0;
-    for(i = 1; i <= boardheight; i++){
-        for(j = 1; j <= boardwidth; j++){
+    for(i = 1; i <= settings.boardheight; i++){
+        for(j = 1; j <= settings.boardwidth; j++){
             if(board[i][j].st != TOUCHED)n++;
         }
     }
-    if(n == minenum)state = WIN;
+    if(n == settings.minenum)state = WIN;
 }
